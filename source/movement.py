5772d506e5cdd3a3ef43c3fb326ed12542cf310f
Karel Čapek <capekk23@spstrutnov.cz>
2024-10-13 22:23:47 +0200

    prepsane kolize
    dneska jeste nefunguje :) - (zitra uvarim)

--- source/movement.py
+++ source/movement.py
@@ -72,7 +72,7 @@ class character(pygame.sprite.Sprite):
 		if pressed[pygame.K_RCTRL]:
 			CharacterVyska = 50
 
-		self.pos = self.pos + self.move * time_passed	#kalkulace pohybu pana Kalkulatora
+		self.pos = self.pos + self.move * time_passed	#kalkulace pohybu
 		self.move.x *= 0.8	#Treni
 		self.move.y += 5000 * time_passed	#Gravitace
 
@@ -97,8 +97,8 @@ class character(pygame.sprite.Sprite):
 
 Hrac = character(100, 700, 100, OnGround = False, CharacterSirka = 50, CharacterVyska = 150)
 
-allSprites = pygame.sprite.GroupSingle()
-allSprites.add(Hrac)
+HracSprite = pygame.sprite.GroupSingle()
+HracSprite.add(Hrac)
 
 class environmentblock(pygame.sprite.Sprite):
 	def __init__(self, x, y, sirka, vyska):
@@ -107,6 +107,10 @@ class environmentblock(pygame.sprite.Sprite):
 		self.move = pygame.math.Vector2()
 		self.sirka = sirka
 		self.vyska = vyska
+		self.Top_col = False
+		self.Bot_col = False
+		self.Lef_col = False
+		self.Rig_col = False
 		self.image = pygame.Surface((self.sirka, self.vyska))
 		self.image.fill((255, 255, 255)) 
 		self.rect = self.image.get_rect(midbottom=(round(self.pos.x), round(self.pos.y)))
@@ -114,11 +118,13 @@ class environmentblock(pygame.sprite.Sprite):
 
 AllCaveSprites = pygame.sprite.Group()
 Bloky = (environmentblock(300, 680, 100, 20),environmentblock(850, 480, 100, 20),environmentblock(600, 610, 100, 20),environmentblock(850, 180, 100, 20),
-		 environmentblock(640, 710, 1280, 20))
+		 environmentblock(640, 710, 1280, 20),
+		 environmentblock(720, 690, 100, 200))
 AllCaveSprites.add(Bloky)
+print(AllCaveSprites)
 
 OnTopBlock = True #jak vim ze je postava na hore na bloku a nemam aplikovat teleport do strany
-jumping = False 
+jumping = False
 
 class Enemy(pygame.sprite.Sprite): #toto bude nepritel
 	def __init__(self, x, y, OnGround):
@@ -126,22 +132,48 @@ class Enemy(pygame.sprite.Sprite): #toto bude nepritel
 		self.pos = pygame.math.Vector2(x, y)
 		self.move = pygame.math.Vector2()
 		self.OnGround = OnGround
-#		self.image = pygame.image.load('character.png').convert_alpha()
-		self.image = pygame.Surface((CharacterSirka, CharacterVyska))
-		self.image.fill((255, 0, 0)) 
+		self.image = pygame.Surface((80, 200))
+		self.image.fill((200, 0, 200)) 
 		self.rect = self.image.get_rect(midbottom = (round(self.pos.x), round(self.pos.y)))
 		self.GroundSpeed = 235	#Rychlost pohybu
-    def patrol():
-        self.move.x += self.GroundSpeed
-    def hunt():
+
+	def patrol(self):
+		pressed = pygame.key.get_pressed()
+		if pressed[pygame.K_HOME]:
+			self.move.x -= self.GroundSpeed
+
+		self.pos = self.pos + self.move * time_passed	#kalkulace pohybu
+		self.move.x *= 0.8	#Treni
+		self.move.y += 5000 * time_passed	#Gravitace
+		self.rect = self.image.get_rect(midbottom = (round(self.pos.x), round(self.pos.y)))
+		if self.rect.left < 0:	#kontrola kolize se zdi nalevo
+			self.rect.left = 0
+			self.pos.x = self.rect.centerx
+		if self.rect.right > 1280:	#kontrola kolize se zdi napravo
+			self.rect.right = 1280
+			self.pos.x = self.rect.centerx
+
+		if self.rect.bottom >= 720:	#kolize s podlahou
+			self.rect.bottom = 720
+			self.move.y = 0  # Tohle resetne pohyb smerem dolu
+
+	#def hunt(self):
         #showdown
-        print("1889")
+		#print("1889")
+
+	def killCheck(self):
+		kolizeCheck = pygame.sprite.spritecollide(Hrac, EnemySprite, False)
+		if kolizeCheck:
+			print('ted ma hrac umrit')
 
 
-ActualInstance = Enemy(800, 700, True)
-EnemySprite = pygame.sprite.Single()
-EnemySprite.add(ActualInstance)
-        
+
+Nepritel = (Enemy(1000, 700, True))
+EnemySprite = pygame.sprite.GroupSingle()
+EnemySprite.add(Nepritel)
+
+KolizovanyBloky = []
+
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -150,47 +182,71 @@ while running:
 	screen.fill((0,0,0))
 	time_passed = clock.tick(60) / 1000.0
 
-	KolizovanySprite = pygame.sprite.spritecollide(Hrac, AllCaveSprites, False)
-
-	
 	#KOLIZE SE ZDMI
 	Hrac.rect.centerx = round(Hrac.pos.x)
 	Hrac.rect.centery = round(Hrac.pos.y)
-	for sprite in KolizovanySprite:
-		if not OnTopBlock and not jumping: #pokud hrac neni na vrchu bloku
-			if Hrac.move.x > 0:
-				if Hrac.rect.right > sprite.rect.left and not jumping:
-					Hrac.rect.right = sprite.rect.left  
-					Hrac.move.x = 0  
-					Hrac.pos.x = Hrac.rect.centerx #sjednoceni logicke a graficke casti hrace
-#					print('kolize v levo')
-			if Hrac.move.x < 0:
-				if Hrac.rect.left < sprite.rect.right and not jumping:
-					Hrac.rect.left = sprite.rect.right
-					Hrac.move.x = 0
-					Hrac.pos.x = Hrac.rect.centerx
-#					print('kolize v pravo')
+	Obdelniky = [block.rect for block in Bloky]
 
-		if Hrac.move.y > 0: #pohyb dolu
-			if Hrac.rect.bottom > sprite.rect.top:
-				Hrac.rect.bottom = sprite.rect.top
-				Hrac.move.y = 0
-				Hrac.pos.y = Hrac.rect.bottom +1
-				Hrac.OnGround = True
-				OnTopBlock = True
-#				print('kolize dole')
-		elif Hrac.move.y < 0: #pohyb nahoru
-			if Hrac.rect.top > sprite.rect.bottom:
-				Hrac.rect.top = sprite.rect.bottom
-				Hrac.move.y *= -1
-				Hrac.pos.y = Hrac.rect.bottom
-#				print('kolize nahore')
-		else:
-			OnTopBlock = False
+	IndexKolizi = Hrac.rect.collidelist(Obdelniky)
 
-	allSprites.update(time_passed)
+	if IndexKolizi >= 0:  #Pokud nastane kolize
+		if IndexKolizi not in KolizovanyBloky:  #CHeck jestli už není v listu kolizovanejch bloku
+			KolizovanyBloky.append(IndexKolizi)
+			print(f"do tohoto bloku narazil: {IndexKolizi}")
+		print(f"Techto se dotyka: {KolizovanyBloky}")
+		for index, sprite in enumerate(AllCaveSprites):
+#			print(f"Index: {index}")
+			if index == IndexKolizi:
+				if not OnTopBlock and not jumping: #pokud hrac neni na vrchu bloku
+					if Hrac.move.x > 0: #KOLIZE V LEVO
+						if Hrac.rect.right > sprite.rect.left and not jumping:
+							sprite.Lef_col = True
+							Hrac.rect.right = sprite.rect.left
+							Hrac.move.x = 0  
+							Hrac.pos.x = Hrac.rect.centerx #sjednoceni logicke a graficke casti hrace
+						else:
+							sprite.Lef_col = False
+					elif Hrac.move.x < 0: 		#KOLIZE V PRAVO
+						if Hrac.rect.left < sprite.rect.right and not jumping:
+							sprite.Rig_col = True
+							Hrac.rect.left = sprite.rect.right
+							Hrac.move.x = 0
+							Hrac.pos.x = Hrac.rect.centerx
+						else: 
+							sprite.Rig_col = False
 
-	allSprites.draw(screen)
+				if Hrac.move.y > 0: #KOLIZE DOLE
+					if Hrac.rect.bottom > sprite.rect.top:
+						sprite.Bot_col = True
+						Hrac.rect.bottom = sprite.rect.top
+						Hrac.move.y = 0
+						Hrac.pos.y = Hrac.rect.bottom +1
+						Hrac.OnGround = True
+						OnTopBlock = True
+					else:
+						sprite.Bot_col = False
+
+				elif Hrac.move.y < 0: #KOLIZE NAHORE
+					if Hrac.rect.top > sprite.rect.bottom:
+						sprite.Top_col = True
+						Hrac.rect.top = sprite.rect.bottom
+						Hrac.move.y *= -1
+						Hrac.pos.y = Hrac.rect.bottom
+					else:
+						sprite.Top_col = False
+				else:
+					OnTopBlock = False
+		KolizovanyBloky.clear()	#clearuje list bloku, do kterych jses narazenej
+
+	Nepritel.patrol()
+	Nepritel.killCheck()
+#	Nepritel.hunt()
+	EnemySprite.draw(screen)
+
+	HracSprite.update(time_passed)
+
+
+	HracSprite.draw(screen)
 	AllCaveSprites.draw(screen)
 
 
